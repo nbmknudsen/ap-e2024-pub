c31fe6c40fd2c96c1161582b63a1f3923d0f0a23
nbmknudsen <93717189+nbmknudsen@users.noreply.github.com>
2024-09-12 10:56:53 +0200

    Week 1
    Solved week 1

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -1,6 +1,90 @@
 module APL.Eval
   (
+  Val(..), eval, envEmpty
   )
 where
 
-import APL.AST ()
+import APL.AST (Exp(..), VName)
+
+data Val
+  = ValInt Integer
+  | ValBool Bool
+  deriving (Eq, Show)
+
+type Error = String
+
+type Env = [(VName, Val)]
+
+-- | Empty environment, which contains no variable bindings.
+envEmpty :: Env
+envEmpty = []
+
+-- | Extend an environment with a new variable binding,
+-- producing a new environment.
+envExtend :: VName -> Val -> Env -> Env
+envExtend name value env = (name, value) : env
+
+-- | Look up a variable name in the provided environment.
+-- Returns Nothing if the variable is not in the environment.
+envLookup :: VName -> Env -> Maybe Val
+envLookup name env = lookup name env
+
+eval_helper :: (Integer -> Integer -> Integer) -> Env -> Exp -> Exp -> Either Error Val
+eval_helper operator env e1 e2 =
+  case (eval env e1, eval env e2) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt x), Right (ValInt y)) -> Right $ ValInt $ x `operator` y
+    (Right _, Right _) -> Left "Wrong type"
+
+
+eval :: Env -> Exp -> Either Error Val
+eval _env (CstInt x) = Right $ (ValInt x)
+eval env (Add e1 e2) = eval_helper (+) env e1 e2
+eval env (Sub e1 e2) = eval_helper (-) env e1 e2
+eval env (Mul e1 e2) = eval_helper (*) env e1 e2
+
+eval env (Div e1 e2) =
+  case (eval env e1, eval env e2) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (_, Right (ValInt 0)) -> Left "Division by zero"
+    (Right (ValInt x), Right (ValInt y)) -> Right $ ValInt $ x `div` y
+    (Right _, Right _) -> Left "Wrong type"
+
+eval env (Pow e1 e2) =
+  case (eval env e1, eval env e2) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt x), Right (ValInt y)) 
+        | y < 0     -> Left "Negative exponent"
+        | otherwise -> Right $ ValInt $ x ^ y
+    (Right _, Right _) -> Left "Wrong type"
+
+eval _env (CstBool x) = Right $ (ValBool x)
+
+eval env (Eql e1 e2) =
+  case (eval env e1, eval env e2) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt x), Right (ValInt y)) -> Right $ ValBool $ x == y
+    (Right (ValBool x), Right (ValBool y)) -> Right $ ValBool $ x == y
+    (Right _, Right _) -> Left "Different types"
+
+eval env (If e1 e2 e3) =
+  case (eval env e1) of
+    Left err -> Left err
+    Right (ValBool True) -> eval env e2
+    Right (ValBool False) -> eval env e3
+    Right _ -> Left "Condition is non-boolean"
+
+eval env (Var n) =
+  case (envLookup n env) of
+    Just x -> Right x
+    Nothing -> Left $ "Unknown variable: " ++ n
+
+eval env (Let n e1 e2) =
+  case (eval env e1) of
+    Left err -> Left err
+    Right x -> eval (envExtend n x env) e2
+
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,12 +1,87 @@
 module APL.Eval_Tests (tests) where
 
-import APL.AST ()
-import APL.Eval ()
+import APL.AST (Exp(..))
+import APL.Eval (Val(..), eval, envEmpty)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit ()
+import Test.Tasty.HUnit (testCase, (@?=))
 
 tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [
+      testCase "Evaluate constant integer" $
+        eval envEmpty (CstInt 42) @?= Right (ValInt 42), 
+
+      testCase "Evaluate constant negative integer" $
+        eval envEmpty (CstInt(-42)) @?= Right (ValInt (-42)), 
+
+      testCase "Evaluate adding" $
+        eval envEmpty (Add (CstInt 2) (CstInt 3)) @?= Right (ValInt 5),
+
+      testCase "Evaluate adding with wrong type" $
+        eval envEmpty (Add (CstBool True) (CstInt 3)) @?= Left "Wrong type",
+
+      testCase "Evaluate subtraction" $
+        eval envEmpty (Sub (CstInt 3) (CstInt 2)) @?= Right (ValInt 1),
+
+      testCase "Evaluate multiplication" $
+        eval envEmpty (Mul (CstInt 3) (CstInt 4)) @?= Right (ValInt 12),
+
+      testCase "Evaluate division" $
+        eval envEmpty (Div (CstInt 12) (CstInt 3)) @?= Right (ValInt 4),
+
+      testCase "Evaluate division by zero" $
+        eval envEmpty (Div (CstInt 12) (CstInt 0)) @?= Left "Division by zero",
+
+      testCase "Evaluate exponentation" $
+        eval envEmpty (Pow (CstInt 2) (CstInt 3)) @?= Right (ValInt 8),
+
+      testCase "Evaluate exponentation with negative exponent" $
+        eval envEmpty (Pow (CstInt 2) (CstInt (-3))) @?= Left "Negative exponent",
+
+      testCase "Evaluate boolean True" $
+        eval envEmpty (CstBool True) @?= Right (ValBool True), 
+
+      testCase "Evaluate boolean False" $
+        eval envEmpty (CstBool False) @?= Right (ValBool False),
+
+      testCase "Evaluate equality Integer True" $
+        eval envEmpty (Eql (CstInt 2) (CstInt 2)) @?= Right (ValBool True), 
+
+      testCase "Evaluate equality Integer False" $
+        eval envEmpty (Eql (CstInt 2) (CstInt 3)) @?= Right (ValBool False),
+
+      testCase "Evaluate equality Boolean True" $
+        eval envEmpty (Eql (CstBool True) (CstBool True)) @?= Right (ValBool True), 
+
+      testCase "Evaluate equality Boolean False" $
+        eval envEmpty (Eql (CstBool False) (CstBool True)) @?= Right (ValBool False),
+
+      testCase "Evaluate equality with different types" $
+        eval envEmpty (Eql (CstBool False) (CstInt 3)) @?= Left "Different types",
+
+      testCase "Evaluate if statement with Integer" $
+        eval envEmpty (If (CstInt 2) (CstInt 3) (CstInt 4)) @?= Left "Condition is non-boolean",
+
+      testCase "Evaluate if statement" $
+        eval envEmpty (If (Eql (CstInt 2) (CstInt 2)) (CstInt 3) (CstInt 4)) @?= Right (ValInt 3),
+
+      testCase "Evaluate Let statement" $
+        eval envEmpty (Let "x" (CstInt 3) (Add (Var "x") (Var "x"))) @?= Right (ValInt 6),
+
+      testCase "Evaluate Let statement with Var" $
+      eval envEmpty (Let "x" (CstInt 3) (Add (Var "x") (Var "y"))) @?= Left "Unknown variable: y"
+
+    ]
+    
+{-,
+      ,
+      testCase "Evaluate subtraction" $
+        eval (Sub (CstInt 3) (CstInt 2)) @?= ValInt 1,
+      testCase "Evaluate multiplication" $
+        eval (Mul (CstInt 3) (CstInt 4)) @?= ValInt 12,
+      testCase "Evaluate division" $
+        eval (Div (CstInt 12) (CstInt 3)) @?= ValInt 4, 
+      testCase "Evaluate exponentation" $
+        eval (Pow (CstInt 2) (CstInt 3)) @?= ValInt 8-}
